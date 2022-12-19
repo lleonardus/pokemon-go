e22bfd184455158af22f1ef28d5b03f80ff6f5b3
leonardus <leonardus.wizard@gmail.com>
2022-12-19 18:22:35 -0300

    first commit

---
+++ src/main/java/com/leonardus/pokemongo/PokemonGoApplication.java
@@ -0,0 +1,13 @@
+package com.leonardus.pokemongo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PokemonGoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PokemonGoApplication.class, args);
+	}
+
+}
